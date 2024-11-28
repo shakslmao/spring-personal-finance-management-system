be1c289b06a341b33bd700e99a28882379ecdd1b
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-11-28 15:03:37 +0100

    user service

---
+++ services/user-service/src/main/java/com/devshaks/personal_finance/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devshaks.personal_finance.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
