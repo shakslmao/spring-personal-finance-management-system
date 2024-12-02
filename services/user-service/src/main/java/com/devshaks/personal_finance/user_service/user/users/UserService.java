ec07f21587fdc7c6ae91f6e21527d8bb9d8791ac
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-02 15:41:46 +0100

    Validation Method

--- services/user-service/src/main/java/com/devshaks/personal_finance/user_service/user/users/UserService.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/user_service/user/users/UserService.java
@@ -17,31 +17,28 @@ public class UserService {
     private final UsernameGenerator usernameGenerator;
     private final PasswordEncoder passwordEncoder;
 
-    @Transactional
-    public User registerUser(@Valid UserRegistrationRequest userRegistrationRequest) {
-        if (userRegistrationRequest == null) {
+    private void validateUserRegistrationRequest(@Valid UserRegistrationRequest registrationRequest) {
+        if (registrationRequest == null) {
             log.error("User registration request is null");
             throw new IllegalArgumentException("User registration request cannot be null");
         }
 
-        if (userRepository.existsByEmail(userRegistrationRequest.email())) {
-            log.error("Email already exists: {}", userRegistrationRequest.email());
+        if (userRepository.existsByEmail(registrationRequest.email())) {
+            log.error("Email already exists: {}", registrationRequest.email());
             throw new IllegalArgumentException("Email already exists");
         }
 
+        if (registrationRequest.dateOfBirth() == null) {
+            log.error("Date of Birth is Null for User: {}", registrationRequest.email());
+            throw new IllegalArgumentException("Date of Birth is required");
+        }
+    }
+
+    @Transactional
+    public User registerUser(@Valid UserRegistrationRequest userRegistrationRequest) {
         try {
+            validateUserRegistrationRequest(userRegistrationRequest);
             User user = userMapper.toUserRegistration(userRegistrationRequest);
-
-            if (user == null) {
-                log.error("User mapping failed: Resulting user is null");
-                throw new RuntimeException("Failed to create user object");
-            }
-
-            if (user.getDateOfBirth() == null) {
-                log.error("Date of birth is null for user: {}", userRegistrationRequest.email());
-                throw new IllegalArgumentException("Date of birth is required");
-            }
-
             String generatedUsername = usernameGenerator.generateUsername(user.getDateOfBirth().getYear());
             log.debug("Generated Username: {}", generatedUsername);
             user.setUsername(generatedUsername);
