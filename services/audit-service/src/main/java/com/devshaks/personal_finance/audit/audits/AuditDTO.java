217936014aab492c840d87c686cd807dbfdf7b5d
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-06 23:55:33 +0100

    Changed Type of Event type to ENUM and changed Type of User ID to LONG.

--- services/audit-service/src/main/java/com/devshaks/personal_finance/audit/audits/AuditDTO.java
+++ services/audit-service/src/main/java/com/devshaks/personal_finance/audit/audits/AuditDTO.java
@@ -3,9 +3,9 @@ package com.devshaks.personal_finance.audit.audits;
 import java.time.LocalDateTime;
 
 public record AuditDTO (
-        String eventType,
+        EventType eventType,
         String serviceName,
-        String userId,
+        Long userId,
         String description,
         LocalDateTime timestamp
 ){
