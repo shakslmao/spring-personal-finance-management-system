a883ca7656253a69928992b1d9ff56211d02f800
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-06 23:59:19 +0100

    User Topic

---
+++ services/user-service/src/main/java/com/devshaks/personal_finance/user_service/user/config/KafkaUserTopicConfig.java
@@ -0,0 +1,16 @@
+package com.devshaks.personal_finance.user_service.user.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaUserTopicConfig {
+    @Bean
+    public NewTopic userTopic() {
+        return TopicBuilder
+                .name("user-topic")
+                .build();
+    }
+}
