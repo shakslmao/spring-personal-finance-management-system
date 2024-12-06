581aa966ac4fa15d0255c9f3b437bb815da748ae
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-06 20:54:41 +0100

    custom audit repo for custom query

---
+++ services/audit-service/src/main/java/com/devshaks/personal_finance/audit/audits/AuditCustomRepository.java
@@ -0,0 +1,39 @@
+package com.devshaks.personal_finance.audit.audits;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class AuditCustomRepository {
+    private final MongoTemplate mongoTemplate;
+
+    public Page<AuditLog> findAll(String serviceName, String userId, String eventType, Pageable pageable) {
+        Criteria criteria = new Criteria();
+
+        if (serviceName != null) {
+            criteria.and("serviceName").is(serviceName);
+        }
+
+        if (userId != null) {
+            criteria.and("userId").is(userId);
+        }
+
+        if (eventType != null) {
+            criteria.and("eventType").is(eventType);
+        }
+
+        Query query = new Query(criteria).with(pageable);
+        List<AuditLog> auditLogs = mongoTemplate.find(query, AuditLog.class);
+        long count = mongoTemplate.count(query.skip(0).limit(0), AuditLog.class);
+        return new PageImpl<>(auditLogs, pageable, count);
+    }
+}
