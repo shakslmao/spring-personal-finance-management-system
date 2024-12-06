3ae151bc185f0aae8d6438c52e7c95526103e966
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-06 20:52:42 +0100

    audit document

---
+++ services/audit-service/src/main/java/com/devshaks/personal_finance/audit/audits/AuditLog.java
@@ -0,0 +1,24 @@
+package com.devshaks.personal_finance.audit.audits;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Document(collation = "audit_logs")
+public class AuditLog {
+
+    @Id
+    private String id;
+    private String eventType; // The type of Event
+    private String serviceName; // The name of the service that generated the Event
+    private String userId; // Associated User ID.
+    private String description; // Details about the Event.
+    private LocalDateTime timestamp; // Timestamp of when the event occurred.
+    private String ipAddress; // IP address associated with the event.
+}
