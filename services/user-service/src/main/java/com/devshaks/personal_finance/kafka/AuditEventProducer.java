04d52fd5f88dc607d7a70694d0836c3e502f7749
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-07 14:37:41 +0100

    class rename

--- services/user-service/src/main/java/com/devshaks/personal_finance/kafka/AuditEventProducer.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/kafka/AuditEventProducer.java
@@ -1,21 +1,24 @@
-package com.devshaks.personal_finance.user_service.user.kafka;
+package com.devshaks.personal_finance.kafka;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuditEventProducer {
-    private final KafkaTemplate<String, AuditEvent> kafkaTemplate;
-
-    public void sendAuditEvent(AuditEvent auditEvent) {
-        Message<AuditEvent> message = MessageBuilder
-                .withPayload(auditEvent)
+    private final KafkaTemplate<String, AuditEvents> kafkaTemplate;
+    public void sendAuditEvent(AuditEvents auditEvents) {
+        log.info("Sending User Event to Audit Service: {}", auditEvents);
+        Message<AuditEvents> message = MessageBuilder
+                .withPayload(auditEvents)
                 .setHeader(KafkaHeaders.TOPIC, "user-topic")
+                .setHeader("__TypeId__", AuditEvents.class.getName())
                 .build();
         kafkaTemplate.send(message);
     }
