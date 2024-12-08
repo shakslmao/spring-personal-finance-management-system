228d1769de12b75896bd8f6296df9b4c43b02c43
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-08 21:58:24 +0100

    admin repo impl

--- services/user-service/src/main/java/com/devshaks/personal_finance/admins/AdminRepository.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/admins/AdminRepository.java
@@ -1,4 +1,11 @@
 package com.devshaks.personal_finance.admins;
 
-public interface AdminRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByEmail(String email);
 }
