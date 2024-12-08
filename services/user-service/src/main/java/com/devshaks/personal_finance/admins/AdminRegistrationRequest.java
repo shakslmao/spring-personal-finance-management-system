d44256f2836b0e245ad99876d4f95dd14edfbd6a
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-08 16:15:49 +0100

    admin registration request

---
+++ services/user-service/src/main/java/com/devshaks/personal_finance/admins/AdminRegistrationRequest.java
@@ -0,0 +1,26 @@
+package com.devshaks.personal_finance.admins;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record AdminRegistrationRequest(
+        @NotNull(message = "First Name is Required")
+        String firstname,
+
+        @NotNull(message = "Email is Required")
+        @Email(message = "Email is Invalid")
+        String email,
+
+        @NotNull(message = "Password is Required")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+        String password,
+
+        @NotNull(message = "Date of Birth is Required")
+        @Past(message = "Date of Birth must be in the past")
+        LocalDate dateOfBirth
+) {
+}
