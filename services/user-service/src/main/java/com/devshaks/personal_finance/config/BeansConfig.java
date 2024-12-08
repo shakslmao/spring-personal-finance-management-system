3df17bfe2fea768a19f4ea61fce4d0d27111ddc9
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-08 21:58:06 +0100

    jwt encoder for user security config, not used atm but will be later

--- services/user-service/src/main/java/com/devshaks/personal_finance/config/BeansConfig.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/config/BeansConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -12,5 +14,11 @@ public class BeansConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        String jwtSetUri = "http://localhost:9098/realms/personal-finance/protocol/openid-connect/certs";
+        return NimbusJwtDecoder.withJwkSetUri(jwtSetUri).build();
     }
 }
