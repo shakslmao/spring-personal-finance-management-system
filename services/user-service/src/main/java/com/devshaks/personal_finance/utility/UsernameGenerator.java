d80e243731d0ff556979f9ab6cd3d25891863792
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-08 21:56:19 +0100

    added new method for admin username generation

--- services/user-service/src/main/java/com/devshaks/personal_finance/utility/UsernameGenerator.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/utility/UsernameGenerator.java
@@ -12,4 +12,11 @@ public class UsernameGenerator {
         int lastTwoDigitsOfYear = yearOfBirth % 100;
         return randomFourDigits + String.format("%02d", lastTwoDigitsOfYear);
     }
+
+    public String generateAdminUsername(int yearOfBirth) {
+        Random random = new Random();
+        int randomFiveDigits = 10000 + random.nextInt(90000);
+        int lastTwoDigitsOfYear = yearOfBirth % 100;
+        return "ADM-" + randomFiveDigits + "-" + lastTwoDigitsOfYear;
+    }
 }
