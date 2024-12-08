7e2b70e143d14755c7664cab321dc8378ca9d422
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-08 16:14:56 +0100

    admin mapper

---
+++ services/user-service/src/main/java/com/devshaks/personal_finance/admins/AdminMapper.java
@@ -0,0 +1,20 @@
+package com.devshaks.personal_finance.admins;
+
+import com.devshaks.personal_finance.users.User;
+import com.devshaks.personal_finance.users.UserRoles;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminMapper {
+    public User toAdminRegistration(AdminRegistrationRequest adminRegistrationRequest) {
+        if (adminRegistrationRequest == null) { throw new IllegalArgumentException("Admin Registration Request is Required"); }
+        if (adminRegistrationRequest.dateOfBirth() == null) { throw new IllegalArgumentException("Date of Birth is Required"); }
+        return User.builder()
+                .firstname(adminRegistrationRequest.firstname())
+                .email(adminRegistrationRequest.email())
+                .password(adminRegistrationRequest.password())
+                .dateOfBirth(adminRegistrationRequest.dateOfBirth())
+                .roles(UserRoles.ADMIN)
+                .build();
+    }
+}
