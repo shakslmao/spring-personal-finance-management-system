b86f5567f58361ce51cbd06277ff85c26ea0e207
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-08 16:59:40 +0100

    x

--- services/user-service/src/main/java/com/devshaks/personal_finance/users/UserController.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/users/UserController.java
@@ -1,6 +1,5 @@
 package com.devshaks.personal_finance.users;
 
-import com.devshaks.personal_finance.admins.AdminRegistrationRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
