48d73905273532aa0a7a05611c4d63ab0336bc8b
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-10 18:35:08 +0100

    removed authorized annotaions for now

--- services/user-service/src/main/java/com/devshaks/personal_finance/admins/AdminController.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/admins/AdminController.java
@@ -23,7 +23,7 @@ public class AdminController {
     private final AdminService adminService;
 
     @PostMapping("/register")
-    //@PreAuthorize("hasRole('SUPER_ADMIN')")
+    //@PreAuthorize("hasRole('SUPER_ADMIN')") or 'USER'
     @Operation(summary = "Register a new Admin")
     public ResponseEntity<AdminDTO> registerAdmin(@RequestBody @Valid AdminRegistrationRequest adminRegistrationRequest) {
         AdminDTO admin = adminService.registerAdmin(adminRegistrationRequest);
@@ -34,5 +34,34 @@ public class AdminController {
         return ResponseEntity.created(location).body(admin);
     }
 
+    // [GET] Get All Users
+
+    // [GET] Retrieve Details For a Specific User
+
+    // [PATCH] Deactivate a User
+
+    // [PATCH] Reactivate a Deactivated User
+
+    // [DELETE] Permanently Delete a User Account
+
+    // [GET] Get All Transactions (Pagination)
+
+    // [GET] Get All Transactions for User
+
+    // [PATCH] Flag Transactions (e.g., Suspicious Activity)
+
+    // [GET] Retrieve all system activity logs.
+
+    // [GET] Retrieve activity logs for a specific user.
+
+    // [POST] Trigger the generation of detailed system reports (e.g., financial insights, admin activities).
+
+    // [GET] Retrieve system metrics such as user growth, transaction volume, and system performance (Prometheus, Grafana)
+
+    // [GET] Check the health of All Microservices (Super Admin)
+
+    // [POST] Send a System-Wide Notification to All Users.
+
+    // [GET] Retrieve All System Alerts (e.g., Budget Threshold Breaches, Suspicious Transactions)
 
 }
