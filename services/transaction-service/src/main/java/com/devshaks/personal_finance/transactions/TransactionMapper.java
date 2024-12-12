9ff9b966817789ef45ad4822bdbc88b206086eac
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-13 00:58:53 +0100

    Transaction Mapper

---
+++ services/transaction-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionMapper.java
@@ -0,0 +1,39 @@
+package com.devshaks.personal_finance.transactions;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class TransactionMapper {
+    public Transactions toNewTransaction(TransactionRequest transactionRequest) {
+        return Transactions.builder()
+                .userId(transactionRequest.userId())
+                .category(transactionRequest.category())
+                .amount(transactionRequest.amount())
+                .description(transactionRequest.description())
+                .tags(TransactionCategoryMapper.getTagsForCategory(transactionRequest.category()))
+                .transactionDate(LocalDateTime.now())
+                .transactionType(TransactionType.EXPENSE)
+                .transactionStatus(TransactionStatus.PENDING)
+                .build();
+    }
+
+    public TransactionDTO toTransactionDTO(Transactions transaction) {
+        if (transaction == null) {
+            throw new IllegalArgumentException("transaction cannot be null");
+        }
+        return new TransactionDTO(
+                transaction.getId(),
+                transaction.getUserId(),
+                transaction.getCategory(),
+                transaction.getAmount(),
+                transaction.getTransactionDate(),
+                transaction.getTransactionType(),
+                transaction.getTransactionStatus(),
+                transaction.getDescription(),
+                transaction.getTags());
+
+    }
+}
