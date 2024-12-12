ea87f0d7c8dbee543eebeb5c7732a9bd6d6e59ab
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-13 00:57:04 +0100

    Transaction Category Mapper

--- services/transaction-service/src/main/java/com/devshaks/personal_finance/TransactionServiceApplication.java
+++ services/transaction-service/src/main/java/com/devshaks/personal_finance/TransactionServiceApplication.java
@@ -3,15 +3,18 @@ package com.devshaks.personal_finance;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableFeignClients
 public class TransactionServiceApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-		SpringApplication.run(TransactionServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+        SpringApplication.run(TransactionServiceApplication.class, args);
+    }
 }
