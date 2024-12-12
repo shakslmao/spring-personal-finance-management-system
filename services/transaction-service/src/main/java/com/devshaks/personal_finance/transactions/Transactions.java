8001e18ea59a1611fbaf856b13f7e285acdd29f1
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-13 00:55:14 +0100

    Transaction Entity

--- services/transaction-service/src/main/java/com/devshaks/personal_finance/transactions/Transactions.java
+++ services/transaction-service/src/main/java/com/devshaks/personal_finance/transactions/Transactions.java
@@ -1,7 +1,53 @@
 package com.devshaks.personal_finance.transactions;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Transactions {
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime transactionDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType transactionType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionStatus transactionStatus;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ElementCollection
+    @CollectionTable(name = "transaction_tags", joinColumns = @JoinColumn(name = "transaction_id"))
+    @Column(name = "tag")
+    private List<String> tags;
 
 }
