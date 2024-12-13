8ba444c81bb2d22f595adbc24eb226f9747f5d8e
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-13 01:00:26 +0100

    Transaction Request

---
+++ services/transaction-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionRequest.java
@@ -0,0 +1,24 @@
+package com.devshaks.personal_finance.transactions;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+public record TransactionRequest(
+        @NotNull(message = "User ID is Required") 
+        Long userId,
+
+        @NotBlank(message = "Category is Required") 
+        String category,
+
+        @NotNull(message = "Amount is Required") 
+        @DecimalMin(value = "0.01", message = "Amount Must be Greater than Zero") 
+        BigDecimal amount,
+
+        @NotBlank(message = "Description is Required") 
+        @Size(max = 255, message = "Description cannot exceed 255 characters") 
+        String description) {
+}
