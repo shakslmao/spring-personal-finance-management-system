cf5bd969e2c5960f2020e8d3c8c2d45e7308fff5
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-13 21:24:17 +0100

    new Transaction Impl

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsService.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsService.java
@@ -1,14 +1,33 @@
 package com.devshaks.personal_finance.transactions;
 
+import com.devshaks.personal_finance.users.UserClientDTO;
+import com.devshaks.personal_finance.users.UserFeignClient;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TransactionsService {
-    public TransactionsDTO newTransaction(@Valid TransactionsRequest transactionRequest) {
-        return null;
+    private final TransactionsRepository transactionsRepository;
+    private final TransactionsMapper transactionsMapper;
+    private final UserFeignClient userFeignClient;
+
+    public TransactionsDTO newTransaction(@Valid TransactionsRequest transactionRequest, Long userId) {
+        log.info("Fetching User Details From User Microservice: {}", userId);
+        UserClientDTO user = userFeignClient.getUserById(userId);
+        if (user == null || user.getId() == null) {
+            log.info("User Not Found for User ID: {}", userId);
+            throw new IllegalArgumentException("User not found");
+        }
+
+        Transactions transactions = transactionsMapper.toNewTransaction(transactionRequest);
+        transactions.setUserId(userId);
+        Transactions savedTransaction = transactionsRepository.save(transactions);
+        return transactionsMapper.toTransactionDTO(savedTransaction);
     }
 
 }
