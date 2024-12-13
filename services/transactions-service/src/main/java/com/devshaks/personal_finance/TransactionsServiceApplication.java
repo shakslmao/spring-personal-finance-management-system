0f82c5ed1f33955b818105f25a94f4ec755fb42e
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-13 21:24:24 +0100

    yes

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/TransactionsServiceApplication.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/TransactionsServiceApplication.java
@@ -3,10 +3,12 @@ package com.devshaks.personal_finance;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class TransactionsServiceApplication {
 
     public static void main(String[] args) {
