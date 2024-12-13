5604a4af32320f0e97619d167b6d6459ab9da6fb
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-13 21:23:33 +0100

    Added User ID as a Parameter

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsController.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsController.java
@@ -3,15 +3,14 @@ package com.devshaks.personal_finance.transactions;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/transactions")
@@ -20,9 +19,10 @@ public class TransactionsController {
 
     private final TransactionsService transactionService;
 
-    @PostMapping("/new")
-    public ResponseEntity<TransactionsDTO> newTransaction(@RequestBody @Valid TransactionsRequest transactionRequest) {
-        TransactionsDTO transaction = transactionService.newTransaction(transactionRequest);
+    @PostMapping("/new/{userId}")
+    public ResponseEntity<TransactionsDTO> newTransaction(@PathVariable("userId") Long userId, @RequestBody @Valid TransactionsRequest transactionRequest) {
+        log.info("Creating new transaction for userId: {} with request: {}", userId, transactionRequest);
+        TransactionsDTO transaction = transactionService.newTransaction(transactionRequest, userId);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(transaction.id())
