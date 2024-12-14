918fc30cfa4337b1861ee9109ba84157b52834ce
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-14 20:02:40 +0100

    Changes

--- services/audit-service/src/main/java/com/devshaks/personal_finance/audits/AuditRepository.java
+++ services/audit-service/src/main/java/com/devshaks/personal_finance/audits/AuditRepository.java
@@ -1,5 +1,8 @@
 package com.devshaks.personal_finance.audits;
 
+import com.devshaks.personal_finance.events.EventType;
+import com.devshaks.personal_finance.events.UserEvents;
+import com.devshaks.personal_finance.kafka.ServiceNames;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -7,6 +10,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AuditRepository extends MongoRepository<Audit, String> {
     Page<Audit> findByUserId(Long userId, Pageable pageable);
-    Page<Audit> findByEventType(EventType eventType, Pageable pageable);
-    Page<Audit> findByServiceName(String serviceName, Pageable pageable);
+    Page<Audit> findByEventType(Enum<?> eventType, Pageable pageable);
+    Page<Audit> findByServiceName(ServiceNames serviceName, Pageable pageable);
 }
