32225ee3a80d1ee51df17aac5f37c76fb97a915d
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-14 16:08:41 +0100

    newTransaction Method working, need to Set up Kafka Events

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsService.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsService.java
@@ -1,6 +1,6 @@
 package com.devshaks.personal_finance.transactions;
 
-import com.devshaks.personal_finance.users.UserClientDTO;
+import com.devshaks.personal_finance.users.UserDetailsResponse;
 import com.devshaks.personal_finance.users.UserFeignClient;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,17 +17,15 @@ public class TransactionsService {
     private final UserFeignClient userFeignClient;
 
     public TransactionsDTO newTransaction(@Valid TransactionsRequest transactionRequest, Long userId) {
-        log.info("Fetching User Details From User Microservice: {}", userId);
-        UserClientDTO user = userFeignClient.getUserById(userId);
-        if (user == null || user.getId() == null) {
-            log.info("User Not Found for User ID: {}", userId);
-            throw new IllegalArgumentException("User not found");
-        }
+        UserDetailsResponse user = userFeignClient.getUserProfileDetails(userId);
+        if (user == null || user.userId() == null) { throw new IllegalArgumentException("User not found"); }
 
         Transactions transactions = transactionsMapper.toNewTransaction(transactionRequest);
         transactions.setUserId(userId);
         Transactions savedTransaction = transactionsRepository.save(transactions);
+        // send kafka event to audit microservice for transaction service fetching user from another service
+        // send kafka event to audit microservice about new transaction for user
+        // send kafka event to user microservice about new transaction for user to their profile.
         return transactionsMapper.toTransactionDTO(savedTransaction);
     }
-
 }
