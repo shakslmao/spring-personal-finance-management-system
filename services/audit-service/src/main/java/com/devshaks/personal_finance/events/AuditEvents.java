c9bf5dbbce6b56bf668f3df4ba71a6bd50412bdb
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-14 20:02:34 +0100

    New Audit Event Enum

---
+++ services/audit-service/src/main/java/com/devshaks/personal_finance/events/AuditEvents.java
@@ -0,0 +1,13 @@
+package com.devshaks.personal_finance.events;
+
+public enum AuditEvents {
+    USER_EVENT,
+    TRANSACTION_EVENT,
+    BUDGET_EVENT,
+    SAVINGS_EVENT,
+    REPORTING_EVENT,
+    NOTIFICATION_EVENT,
+    INVESTMENT_EVENT,
+    CURRENCY_EXCHANGE_EVENT,
+    LOAN_EVENT
+}
