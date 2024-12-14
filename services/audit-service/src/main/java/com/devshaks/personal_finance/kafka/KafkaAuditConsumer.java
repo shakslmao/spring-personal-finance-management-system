b008d6f08931abf5f3971432a86be640d52d0922
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-14 20:04:12 +0100

    Reworked Consumer to Handle Different Events from Various Microservices

--- services/audit-service/src/main/java/com/devshaks/personal_finance/kafka/KafkaAuditConsumer.java
+++ services/audit-service/src/main/java/com/devshaks/personal_finance/kafka/KafkaAuditConsumer.java
@@ -2,42 +2,89 @@ package com.devshaks.personal_finance.kafka;
 
 import com.devshaks.personal_finance.audits.Audit;
 import com.devshaks.personal_finance.audits.AuditRepository;
+import com.devshaks.personal_finance.events.EventMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.function.Function;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaAuditConsumer {
     private final AuditRepository auditRepository;
+    private final EventMapper eventMapper;
+    private final ObjectMapper objectMapper;
+    private Map<Class<?>, Function<Object, Audit>> eventMapperRegistry;
 
-    @KafkaListener(topics = "user-topic", groupId = "auditGroup", containerFactory = "kafkaListenerContainerFactory")
-    public void consumeAuditEvent(@Payload AuditEvents auditEvents) {
-        log.info("Received Audit Event: {}", auditEvents);
-        if (auditEvents == null || auditEvents.eventType() == null || auditEvents.serviceName() == null || auditEvents.timestamp() == null) {
-            log.warn("Invalid Audit Event received: {}", auditEvents);
-            return;
-        }
-        Audit audit = mapToAudit(auditEvents);
+    @KafkaListener(topics = {"user-topic", "transaction-topic", "budget-topic"}, groupId = "auditGroup", containerFactory = "kafkaListenerContainerFactory")
+    public void consumeAuditEvent(@Payload String payload, @Header("kafka_receivedTopic") String topic) {
         try {
+            Object event = parseEventByTopic(payload, topic);
+            Audit audit = mapToAudit(event);
             auditRepository.save(audit);
         } catch (Exception e) {
-            log.error("Error while saving audit event: {}", auditEvents, e);
-            throw new RuntimeException(e);
+            log.error(e.getMessage(), e);
         }
     }
 
+    private Object parseEventByTopic(String payload, String topic) throws JsonProcessingException {
+        return switch (topic) {
+            case "user-topic" -> objectMapper.readValue(payload, UserEventDTO.class);
+            case "transaction-topic" -> objectMapper.readValue(payload, TransactionEventDTO.class);
+            case "budget-topic" -> objectMapper.readValue(payload, BudgetEventDTO.class);
+            default -> throw new IllegalArgumentException("Unsupported Topic: " + topic);
+        };
 
-    private Audit mapToAudit(AuditEvents auditEvents) {
-       return Audit.builder()
-               .eventType(auditEvents.eventType())
-               .serviceName(auditEvents.serviceName())
-               .userId(auditEvents.userId())
-               .description(auditEvents.description())
-               .timestamp(auditEvents.timestamp())
-               .build();
+    }
+
+    @PostConstruct
+    public void initMapperRegistry() {
+        eventMapperRegistry = Map.of(
+                UserEventDTO.class, event -> {
+                    UserEventDTO userEvent = (UserEventDTO) event;
+                    return Audit.builder()
+                            .eventType(eventMapper.mapEventToSpecificType(userEvent.eventType()))
+                            .serviceName(userEvent.serviceName())
+                            .userId(userEvent.userId())
+                            .description(userEvent.description())
+                            .timestamp(userEvent.timestamp())
+                            .build();
+                },
+                TransactionEventDTO.class, event -> {
+                    TransactionEventDTO transactionEvent = (TransactionEventDTO) event;
+                    return Audit.builder()
+                            .eventType(eventMapper.mapEventToSpecificType(transactionEvent.eventType()))
+                            .serviceName(transactionEvent.serviceName())
+                            .userId(transactionEvent.userId())
+                            .description(transactionEvent.description())
+                            .timestamp(transactionEvent.timestamp())
+                            .build();
+                },
+                BudgetEventDTO.class, event -> {
+                    BudgetEventDTO budgetEvent = (BudgetEventDTO) event;
+                    return Audit.builder()
+                            .eventType(eventMapper.mapEventToSpecificType(budgetEvent.eventType()))
+                            .serviceName(budgetEvent.serviceName())
+                            .userId(budgetEvent.userId())
+                            .description(budgetEvent.description())
+                            .timestamp(budgetEvent.timestamp())
+                            .build();
+                }
+        );
+    }
+
+    private Audit mapToAudit(Object event) {
+        Function<Object, Audit> mapper = eventMapperRegistry.get(event.getClass());
+        if (mapper == null) { throw new IllegalArgumentException("Unknown event type: " + event.getClass()); }
+        return mapper.apply(event);
     }
 }
