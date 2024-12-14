0b72a6d7480f59257609b1ea1554ac85913a58ee
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-14 20:04:30 +0100

    Changed to take String as Type to Handle the different DTOS

--- services/audit-service/src/main/java/com/devshaks/personal_finance/config/KafkaListenerConfig.java
+++ services/audit-service/src/main/java/com/devshaks/personal_finance/config/KafkaListenerConfig.java
@@ -1,6 +1,7 @@
 package com.devshaks.personal_finance.config;
 
-import com.devshaks.personal_finance.kafka.AuditEvents;
+import com.devshaks.personal_finance.events.EventType;
+import com.devshaks.personal_finance.kafka.UserEventDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -21,22 +22,22 @@ import java.util.Map;
 public class KafkaListenerConfig {
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, AuditEvents> kafkaListenerContainerFactory(
-            ConsumerFactory<String, AuditEvents> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, AuditEvents> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         return factory;
     }
 
     @Bean
-    public ConsumerFactory<String, AuditEvents> consumerFactory() {
+    public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "auditGroup");
-        config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.devshaks.personal_finance.kafka");
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
-                new JsonDeserializer<>(AuditEvents.class));
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new StringDeserializer());
+
     }
 
 }
