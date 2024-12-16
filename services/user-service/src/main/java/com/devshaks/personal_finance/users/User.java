898ba745d1e196b4be132eac9a7a8d19a68728f7
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-16 22:38:05 +0100

    eagerly fetch transactions.

--- services/user-service/src/main/java/com/devshaks/personal_finance/users/User.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/users/User.java
@@ -57,7 +57,7 @@ public class User {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Transactions> transactions;
 
     @PrePersist
