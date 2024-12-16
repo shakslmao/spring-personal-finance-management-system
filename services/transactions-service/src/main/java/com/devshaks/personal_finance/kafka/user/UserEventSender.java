0207c486371e3e7ca08807faa2a9b98e185b892c
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-16 22:38:50 +0100

    added description fields.

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/kafka/user/UserEventSender.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/kafka/user/UserEventSender.java
@@ -6,35 +6,36 @@ import com.devshaks.personal_finance.kafka.services.ServiceNames;
 import com.devshaks.personal_finance.transactions.TransactionsStatus;
 import com.devshaks.personal_finance.transactions.TransactionsType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.KafkaException;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserEventSender {
     private final UserEventProducer userEventProducer;
-    public void sendEventToUser(TransactionEvents transactionEvents, Long userId, Long transactionId, String description, String category, BigDecimal amount) {
+    public void sendEventToUser(TransactionEvents transactionEvents, Long userId, Long transactionId, String eventDescription, String userDescription, String category, BigDecimal amount){
         try {
-            userEventProducer.sendAuditProducerEventFromTransaction(new UserTransactionEventDTO(
+            userEventProducer.sendUserProducerEventFromTransaction(new UserTransactionEventDTO(
                     transactionId,
                     userId,
                     category,
                     amount,
-                    LocalDateTime.now().toString(),
+                    LocalDateTime.now(),
                     TransactionsType.EXPENSE,
                     TransactionsStatus.PENDING,
-                    description,
+                    userDescription,
+                    eventDescription,
                     ServiceNames.TRANSACTION_SERVICE,
                     transactionEvents
             ));
         } catch (KafkaException e) {
-            // Catch Kafka-specific exceptions
             throw new RuntimeException("Kafka error sending event: " + e.getMessage(), e);
         } catch (Exception e) {
-            // Final catch-all
             throw new RuntimeException("General error sending event to user service: " + e.getMessage(), e);
         }
     }
