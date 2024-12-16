8e56c252698dcff8a987e71cdd4de493507ff362
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-16 22:39:09 +0100

    added description fields.

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/kafka/data/UserTransactionEventDTO.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/kafka/data/UserTransactionEventDTO.java
@@ -5,19 +5,22 @@ import com.devshaks.personal_finance.kafka.services.ServiceNames;
 import com.devshaks.personal_finance.transactions.TransactionsStatus;
 import com.devshaks.personal_finance.transactions.TransactionsType;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public record UserTransactionEventDTO(
-        @JsonProperty("transactionId") Long transactionId,
-        @JsonProperty("userId") Long userId,
-        @JsonProperty("category") String category,
-        @JsonProperty("amount") BigDecimal amount,
-        @JsonProperty("transactionDate") String transactionDate,
-        @JsonProperty("transactionType") TransactionsType transactionsType,
-        @JsonProperty("transactionStatus") TransactionsStatus transactionsStatus,
-        @JsonProperty("description") String description,
-        @JsonProperty("serviceNames") ServiceNames serviceNames,
-        @JsonProperty("eventType") TransactionEvents eventType
+        @NotNull @JsonProperty("transactionId") Long transactionId,
+        @NotNull @JsonProperty("userId") Long userId,
+        @NotNull @JsonProperty("category") String category,
+        @NotNull @JsonProperty("amount") BigDecimal amount,
+        @NotNull @JsonProperty("transactionDate") LocalDateTime transactionDate,
+        @NotNull @JsonProperty("transactionType") TransactionsType transactionsType,
+        @NotNull @JsonProperty("transactionStatus") TransactionsStatus transactionsStatus,
+        @NotNull @JsonProperty("transactionDescription") String transactionDescription,
+        @NotNull @JsonProperty("eventDescription") String eventDescription,
+        @NotNull @JsonProperty("serviceNames") ServiceNames serviceNames,
+        @NotNull @JsonProperty("eventType") TransactionEvents eventType
 ) {
 }
