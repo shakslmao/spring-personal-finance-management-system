53681f1b8131707818bf19617f93471caa75b345
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-18 23:15:37 +0100

    updated repo for stat method

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsRepository.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsRepository.java
@@ -1,9 +1,13 @@
 package com.devshaks.personal_finance.transactions;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-public interface TransactionsRepository extends JpaRepository<Transactions, Long> {
+public interface TransactionsRepository extends JpaRepository<Transactions, Long>, JpaSpecificationExecutor<Transactions> {
     List<Transactions> findByUserId(Long userId);
+    List<Transactions> findByUserIdAndCategory(Long userId, String category);
+    List<Transactions> findByUserIdAndTransactionDateBetween(Long userId, LocalDateTime start, LocalDateTime end);
 }
