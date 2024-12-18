cecc1fecf185a013df128578b9549f96baaadc6e
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-18 23:16:42 +0100

    @Redis Caching

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/TransactionsServiceApplication.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/TransactionsServiceApplication.java
@@ -3,12 +3,14 @@ package com.devshaks.personal_finance;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableCaching
 public class TransactionsServiceApplication {
 
     public static void main(String[] args) {
