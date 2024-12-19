24dd394da69dc7f8d1882bf7768ea4b28610fee0
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-19 18:19:07 +0100

    create new budger for user

---
+++ services/budget-service/src/main/java/com/devshaks/personal_finance/budget/BudgetService.java
@@ -0,0 +1,37 @@
+package com.devshaks.personal_finance.budget;
+
+import com.devshaks.personal_finance.user.UserDetailsResponse;
+import com.devshaks.personal_finance.user.UserFeignClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BudgetService {
+    private final BudgetRepository budgetRepository;
+    private final BudgetMapper budgetMapper;
+    private final UserFeignClient userFeignClient;
+
+    @Transactional
+    public BudgetResponse createUserBudget(Long userId, BudgetRequest budgetRequest) {
+        log.info("Fetching user details for userId: {}", userId);
+        UserDetailsResponse userDetails;
+        try {
+            userDetails =  userFeignClient.getUserProfileDetails(userId);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to fetch user details", e);
+        }
+
+        if (userDetails == null || userDetails.userId() == null) {
+            throw new RuntimeException("User not found");
+        }
+
+        Budget budget = budgetMapper.toNewBudget(budgetRequest);
+        budget.setUserId(userId);
+        Budget savedBudget = budgetRepository.save(budget);
+        return budgetMapper.mapBudgetToResponse(savedBudget);
+    }
+}
