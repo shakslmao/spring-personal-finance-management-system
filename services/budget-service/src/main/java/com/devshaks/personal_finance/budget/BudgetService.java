cc2d5a0543a7accbd2c2b55d505d1f8baa4baee4
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-19 22:31:56 +0100

    budget event for audit ms

--- services/budget-service/src/main/java/com/devshaks/personal_finance/budget/BudgetService.java
+++ services/budget-service/src/main/java/com/devshaks/personal_finance/budget/BudgetService.java
@@ -1,5 +1,7 @@
 package com.devshaks.personal_finance.budget;
 
+import com.devshaks.personal_finance.kafka.audit.AuditEventSender;
+import com.devshaks.personal_finance.kafka.events.BudgetEvents;
 import com.devshaks.personal_finance.user.UserDetailsResponse;
 import com.devshaks.personal_finance.user.UserFeignClient;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ public class BudgetService {
     private final BudgetRepository budgetRepository;
     private final BudgetMapper budgetMapper;
     private final UserFeignClient userFeignClient;
+    private final AuditEventSender auditEventSender;
 
     @Transactional
     public BudgetResponse createUserBudget(Long userId, BudgetRequest budgetRequest) {
@@ -25,13 +28,11 @@ public class BudgetService {
             throw new RuntimeException("Failed to fetch user details", e);
         }
 
-        if (userDetails == null || userDetails.userId() == null) {
-            throw new RuntimeException("User not found");
-        }
-
+        if (userDetails == null || userDetails.userId() == null) { throw new RuntimeException("User not found"); }
         Budget budget = budgetMapper.toNewBudget(budgetRequest);
         budget.setUserId(userId);
         Budget savedBudget = budgetRepository.save(budget);
+        auditEventSender.sendEventToAudit(BudgetEvents.BUDGET_CREATED, userId, "User Set a New Budget");
         return budgetMapper.mapBudgetToResponse(savedBudget);
     }
 }
