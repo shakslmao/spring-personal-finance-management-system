4cd36e091ced32d4700a2700c6c31cf5686d2470
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-20 21:20:17 +0100

    get by id / get by user id impl

--- services/budget-service/src/main/java/com/devshaks/personal_finance/budget/BudgetController.java
+++ services/budget-service/src/main/java/com/devshaks/personal_finance/budget/BudgetController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -27,4 +27,42 @@ public class BudgetController {
         BudgetResponse budgetResponse = budgetService.createUserBudget(userId, budgetRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(budgetResponse);
     }
-}
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get Budget Details by Budget ID")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Budget Found"),  @ApiResponse(responseCode = "404", description = "Budget Not Found")})
+    public ResponseEntity<BudgetResponse> getBudgetById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(budgetService.getBudgetById(id));
+    }
+
+    @GetMapping("/user/{userId}")
+    @Operation(summary = "Get a Budget Response From a User ID.")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Budgets Found"), @ApiResponse(responseCode = "404", description = "No Budgets Found for User")})
+    public ResponseEntity<List<BudgetResponse>> getUserBudgets(@PathVariable("userId") Long userId) {
+        List <BudgetResponse> response = budgetService.getUserBudgets(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    // Delete a Budget
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Budget by Budget ID")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Budget Deleted Successfully"),  @ApiResponse(responseCode = "404", description = "Budget Not Found")})
+    public ResponseEntity<String> deleteBudget(@PathVariable("id") Long id) {
+        budgetService.deleteBudget(id);
+        return ResponseEntity.ok("Budget Deleted Successfully");
+    }
+
+
+    // Update an Existing Budget
+
+    // Add a new Category to an Existing Budget
+
+    // Update a Category from a Budget
+
+    // Delete a Category from a Budget
+
+    // Check if a Transaction Exceeds a Budget
+
+    // Generate Budget Report.
+
+  }
--- services/budget-service/src/main/java/com/devshaks/personal_finance/budget/BudgetService.java
+++ services/budget-service/src/main/java/com/devshaks/personal_finance/budget/BudgetService.java
@@ -1,5 +1,8 @@
 package com.devshaks.personal_finance.budget;
 
+import com.devshaks.personal_finance.exceptions.AuditEventException;
+import com.devshaks.personal_finance.exceptions.BudgetNotFoundException;
+import com.devshaks.personal_finance.exceptions.UserNotFoundException;
 import com.devshaks.personal_finance.kafka.audit.AuditEventSender;
 import com.devshaks.personal_finance.kafka.events.BudgetEvents;
 import com.devshaks.personal_finance.user.UserDetailsResponse;
@@ -8,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -20,7 +26,6 @@ public class BudgetService {
 
     @Transactional
     public BudgetResponse createUserBudget(Long userId, BudgetRequest budgetRequest) {
-        log.info("Fetching user details for userId: {}", userId);
         UserDetailsResponse userDetails;
         try {
             userDetails =  userFeignClient.getUserProfileDetails(userId);
@@ -28,11 +33,41 @@ public class BudgetService {
             throw new RuntimeException("Failed to fetch user details", e);
         }
 
-        if (userDetails == null || userDetails.userId() == null) { throw new RuntimeException("User not found"); }
+        if (userDetails == null || userDetails.userId() == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        String month = budgetRequest.month().toString();
+        budgetRepository.findByUserIdAndMonth(userId, month).ifPresent(existingBudget -> {
+            throw new RuntimeException("Budget already exists for this Month");
+        });
+
         Budget budget = budgetMapper.toNewBudget(budgetRequest);
         budget.setUserId(userId);
         Budget savedBudget = budgetRepository.save(budget);
-        auditEventSender.sendEventToAudit(BudgetEvents.BUDGET_CREATED, userId, "User Set a New Budget");
+        try {
+            auditEventSender.sendEventToAudit(BudgetEvents.BUDGET_CREATED, userId, "User Set a New Budget");
+
+        } catch (Exception e) {
+            throw new AuditEventException("Failed to send Event to Audit.");
+        }
         return budgetMapper.mapBudgetToResponse(savedBudget);
+    }
+
+    public BudgetResponse getBudgetById(Long id) {
+        return budgetRepository.findById(id)
+                .map(budgetMapper::mapBudgetToResponse)
+                .orElseThrow(() -> new BudgetNotFoundException("Budget Was Not Found"));
+    }
+
+    public List<BudgetResponse> getUserBudgets(Long userId) {
+        List<Budget> budgets = budgetRepository.findByUserId(userId);
+        if (budgets.isEmpty()) { throw new BudgetNotFoundException("Budget Was Not Found"); }
+        return budgets.stream()
+                .map(budgetMapper::mapBudgetToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteBudget(Long id) {
+        budgetRepository.deleteById(id);
     }
 }
