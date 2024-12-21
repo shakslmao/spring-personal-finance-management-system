ac46ee57bcbc9de33386623fb6700729e7237187
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-21 16:36:22 +0100

    changes

--- services/budget-service/src/main/java/com/devshaks/personal_finance/exceptions/BudgetUpdateException.java
+++ services/budget-service/src/main/java/com/devshaks/personal_finance/exceptions/BudgetUpdateException.java
@@ -1,7 +1,10 @@
 package com.devshaks.personal_finance.exceptions;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class BudgetUpdateException extends RuntimeException {
-  public BudgetUpdateException(String message) {
-    super(message);
-  }
+    public final String exceptionMessage;
 }
--- services/budget-service/src/main/java/com/devshaks/personal_finance/user/UserFeignClient.java
+++ services/budget-service/src/main/java/com/devshaks/personal_finance/user/UserFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${application.config.user-service-url}")
+@FeignClient(name = "user-service", url = "${application.config.user-service-url}" )
 public interface UserFeignClient {
     @GetMapping("/{userId}")
     UserDetailsResponse getUserProfileDetails(@PathVariable("userId") Long userId);
