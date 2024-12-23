23207b088f647d4ffc17fae254daf897b9d0900a
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-23 18:10:55 +0100

    budget feign impl

---
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/budget/BudgetFeignClient.java
@@ -0,0 +1,14 @@
+package com.devshaks.personal_finance.budget;
+
+import jakarta.ws.rs.Path;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "budget-service", url = "${application.config.budget-service-url}")
+public interface BudgetFeignClient {
+    @GetMapping("/user/{userId}")
+    List<BudgetResponse> getUserBudgets(@PathVariable("userId") Long userId);
+}
---
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/budget/BudgetResponse.java
@@ -0,0 +1,18 @@
+package com.devshaks.personal_finance.budget;
+
+import com.devshaks.personal_finance.budget.BudgetCategoryResponse;
+
+import java.math.BigDecimal;
+import java.time.YearMonth;
+import java.util.List;
+
+public record BudgetResponse(
+        Long id,
+        Long userId,
+        BigDecimal monthlyLimit,
+        BigDecimal spentAmount,
+        BigDecimal remainingAmount,
+        YearMonth month,
+        List<BudgetCategoryResponse> categories
+) {
+}
