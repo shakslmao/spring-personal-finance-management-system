1b63528b0f664c03f21d8987f46339e3f723da0d
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-23 18:12:06 +0100

    changed transaction method to throw 4xx, 5xx if user attempts to make a transaction that exceeds their monthly budgets.

--- services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsService.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionsService.java
@@ -1,5 +1,8 @@
 package com.devshaks.personal_finance.transactions;
 
+import com.devshaks.personal_finance.budget.BudgetCategoryResponse;
+import com.devshaks.personal_finance.budget.BudgetFeignClient;
+import com.devshaks.personal_finance.budget.BudgetResponse;
 import com.devshaks.personal_finance.exceptions.BudgetExceededException;
 import com.devshaks.personal_finance.exceptions.BudgetNotFoundException;
 import com.devshaks.personal_finance.exceptions.TransactionNotFoundException;
@@ -13,7 +16,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.apache.kafka.common.KafkaException;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,12 +24,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.devshaks.personal_finance.kafka.events.TransactionEvents.TRANSACTION_CREATED;
+import static com.devshaks.personal_finance.kafka.events.TransactionEvents.TRANSACTION_FAILED_BUDGET_EXCEEDED;
 
 @Slf4j
 @Service
@@ -36,15 +41,56 @@ public class TransactionsService {
     private final TransactionsRepository transactionsRepository;
     private final TransactionsMapper transactionsMapper;
     private final UserFeignClient userFeignClient;
+    private final BudgetFeignClient budgetFeignClient;
     private final AuditEventSender auditEventSender;
     private final UserEventSender userEventSender;
     private final TransactionEventSender transactionEventSender;
 
     public TransactionsDTO newTransaction(@Valid TransactionsRequest transactionRequest, Long userId) {
+        // Confirm user exists
         UserDetailsResponse user = userFeignClient.getUserProfileDetails(userId);
         if (user == null || user.userId() == null) {
             throw new UserNotFoundException("User not found");
         }
+
+        // Get all budgets for this user
+        List<BudgetResponse> budgets = budgetFeignClient.getUserBudgets(userId);
+        if (budgets == null || budgets.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "No budgets found for user");
+        }
+
+        // find the current month budget
+        YearMonth currentMonth = YearMonth.now();
+        BudgetResponse currentMonthBudget = budgets.stream()
+                .filter(b -> b.month().equals(currentMonth))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.CONFLICT, "No budget found for the current month"));
+
+        // check if the transaction amount exceeds remaining budget
+        BigDecimal transactionAmount = transactionRequest.amount();
+        if (currentMonthBudget != null && currentMonthBudget.remainingAmount().compareTo(transactionAmount) < 0) {
+            auditEventSender.sendEventToAudit(TRANSACTION_FAILED_BUDGET_EXCEEDED, userId, "Failed Transaction, User Attempted to Breach Budget");
+            throw new BudgetExceededException("Monthly Budget exceeded");
+        }
+
+        String transactionCategory = transactionRequest.category();
+        if (transactionCategory != null) {
+            assert currentMonthBudget != null;
+            BudgetCategoryResponse category = currentMonthBudget.categories().stream()
+                    .filter(cat -> cat.name().equalsIgnoreCase(transactionCategory))
+                    .findFirst()
+                    .orElse(null);
+
+            if (category != null && category.categoryLimit() != null) {
+                BigDecimal categoryRemaining = category.categoryLimit().subtract(category.spentAmount());
+                if (categoryRemaining.compareTo(transactionAmount) < 0) {
+                    auditEventSender.sendEventToAudit(TRANSACTION_FAILED_BUDGET_EXCEEDED, userId, "Failed Transaction, User Attempted to Breach Budget");
+                    throw new BudgetExceededException("Budget Limit Exceeded For Category");
+                }
+            }
+        }
+
         Transactions transactions = transactionsMapper.toNewTransaction(transactionRequest);
         transactions.setUserId(userId);
         transactions.setTransactionStatus(TransactionsStatus.PENDING);
@@ -88,8 +134,7 @@ public class TransactionsService {
                 .orElseThrow(() -> new TransactionNotFoundException("Cannot Find Transaction With ID"));
     }
 
-    // @Cacheable(value = "transactions", key = "T(String).format('%d-%s', #userId,
-    // #category)")
+    // @Cacheable(value = "transactions", key = "T(String).format('%d-%s', #userId, #category)")
     public List<TransactionsDTO> getUserTransactionByCategory(Long userId, String category) {
         List<Transactions> transactions = transactionsRepository.findByUserIdAndCategory(userId, category);
         if (transactions.isEmpty()) {
@@ -98,8 +143,7 @@ public class TransactionsService {
         return transactions.stream().map(transactionsMapper::toTransactionDTO).toList();
     }
 
-    // @Cacheable(value = "transaction-filters", key = "#userId + '-' + #category +
-    // '-' + #pageable.pageNumber")
+    // @Cacheable(value = "transaction-filters", key = "#userId + '-' + #category + '-' + #pageable.pageNumber")
     public PaginatedTransactionDTO getTransactionFilter(Long userId, String category, LocalDateTime transactionDate,
             TransactionsType transactionsType, TransactionsStatus transactionsStatus, Pageable pageable) {
         Specification<Transactions> specification = Specification.where(null);
