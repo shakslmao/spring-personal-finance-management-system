f694e3aad952a753bfcb16bb503e4b7fa5cf1b53
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-25 18:11:10 +0100

    payment microservice

---
+++ services/payment-service/src/main/java/com/devshaks/personal_finance/PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devshaks.personal_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+}
