c741894ec405ba8db7ac23b5c81f868c1c083c0a
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-27 21:57:53 +0100

    Payment Kafka Event DTO.

---
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/kafka/data/PaymentTransactionEventDTO.java
@@ -0,0 +1,13 @@
+package com.devshaks.personal_finance.kafka.data;
+
+import com.devshaks.personal_finance.transactions.PaymentStatus;
+
+import java.math.BigDecimal;
+
+public record PaymentTransactionEventDTO(
+        Long transactionId,
+        Long userId,
+        BigDecimal amount,
+        PaymentStatus status
+) {
+}
