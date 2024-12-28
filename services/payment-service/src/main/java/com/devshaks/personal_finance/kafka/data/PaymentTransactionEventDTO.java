939610cc61e9dc8a9f80a220cf01d706998b2d06
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-28 15:11:22 +0100

    changes

--- services/payment-service/src/main/java/com/devshaks/personal_finance/kafka/data/PaymentTransactionEventDTO.java
+++ services/payment-service/src/main/java/com/devshaks/personal_finance/kafka/data/PaymentTransactionEventDTO.java
@@ -2,14 +2,16 @@ package com.devshaks.personal_finance.kafka.data;
 
 
 import com.devshaks.personal_finance.payments.PaymentStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 
 public record PaymentTransactionEventDTO(
-        Long transactionId,
-        Long userId,
-        BigDecimal amount,
-        PaymentStatus status
+        @NotNull @JsonProperty("transactionId") Long transactionId,
+        @NotNull @JsonProperty("userId") Long userId,
+        @NotNull @JsonProperty("amount") BigDecimal amount,
+        @NotNull @JsonProperty("status") PaymentStatus status
 ) {
 }
--- services/transactions-service/src/main/java/com/devshaks/personal_finance/kafka/data/PaymentTransactionEventDTO.java
+++ services/transactions-service/src/main/java/com/devshaks/personal_finance/kafka/data/PaymentTransactionEventDTO.java
@@ -1,13 +1,15 @@
 package com.devshaks.personal_finance.kafka.data;
 
 import com.devshaks.personal_finance.transactions.PaymentStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record PaymentTransactionEventDTO(
-        Long transactionId,
-        Long userId,
-        BigDecimal amount,
-        PaymentStatus status
+        @NotNull @JsonProperty("transactionId") Long transactionId,
+        @NotNull @JsonProperty("userId") Long userId,
+        @NotNull @JsonProperty("amount") BigDecimal amount,
+        @NotNull @JsonProperty("status") PaymentStatus status
 ) {
 }
