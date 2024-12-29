ff7ec95acffe56f368dee8772c4e601a4cd061e5
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-29 21:56:34 +0100

    minor changes

--- services/payment-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionFeignClient.java
+++ services/payment-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionFeignClient.java
@@ -1,10 +1,14 @@
 package com.devshaks.personal_finance.transactions;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "transaction-service", url = "${application.config.transaction-service-url}")
+import com.devshaks.personal_finance.config.FeignConfig;
+
+@RefreshScope
+@FeignClient(name = "transaction-service", url = "${application.config.transaction-service-url}", configuration = FeignConfig.class)
 public interface TransactionFeignClient {
 
     @GetMapping("/{id}")
