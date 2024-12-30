6d2764836e9ef449a7d2951f05d078e78983ea16
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-12-30 15:09:47 +0100

    added FeignConfig.class to @FeignClient to Allow Authentication.

--- services/payment-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionFeignClient.java
+++ services/payment-service/src/main/java/com/devshaks/personal_finance/transactions/TransactionFeignClient.java
@@ -1,11 +1,10 @@
 package com.devshaks.personal_finance.transactions;
 
+import com.devshaks.personal_finance.config.FeignConfig;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import com.devshaks.personal_finance.config.FeignConfig;
 
 @RefreshScope
 @FeignClient(name = "transaction-service", url = "${application.config.transaction-service-url}", configuration = FeignConfig.class)
