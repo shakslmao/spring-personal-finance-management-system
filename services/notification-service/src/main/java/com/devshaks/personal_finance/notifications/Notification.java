b4fef9d28595cc3df1bf57d6fd1f2d5161bf2da9
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-06 15:35:50 +0000

    notification document

--- services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/Notification.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/Notification.java
@@ -1,7 +1,12 @@
 package com.devshaks.personal_finance.notifications;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.devshaks.personal_finance.NotificationType;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,12 +17,26 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
-@Document
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "notifications")
 public class Notification {
 
     @Id
     private String id;
-
+    private String recipientId;
+    private NotificationType notificationType;
+    private String notificationMessage;
+    private Map<String, String> metaData;
+    private NotificationStatus notificationStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime sentAt;
+    private LocalDateTime readAt;
 }
+
+// FCM for Notifications - Configure
+// Twilio for SMS - Configure
+// SES for Email - Configure
+
+// Websockets - FCM Notifications
+// Kafka for Email & SMS.
