d93935106574e6b6703869d2d2f811cdb6965519
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-06 19:18:27 +0000

    not filled yet but service methods

--- services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationService.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationService.java
@@ -1,5 +1,7 @@
 package com.devshaks.personal_finance.notifications;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -7,5 +9,21 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
+
+    public NotificationResponse createNotification(NotificationRequest request) {
+        return null;
+    }
+
+    public void sendNotification(String id) {
+
+    }
+
+    public void markNotificationAsRead(String id) {
+
+    }
+
+    public List<NotificationResponse> getNotificationsByRecipient(String recipientId, NotificationStatus status) {
+        return null;
+    }
 
 }
