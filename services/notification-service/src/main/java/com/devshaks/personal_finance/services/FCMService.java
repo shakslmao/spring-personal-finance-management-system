4ec967f9c771f8658bfd8e15047b06f5fc98184c
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-06 20:03:51 +0000

    updates fcm service

--- services/notification-service/src/main/java/com/devshaks/personal_finance/services/FCMService.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/services/FCMService.java
@@ -1,16 +1,21 @@
 package com.devshaks.personal_finance.services;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class FCMService {
+
+    private final SimpMessagingTemplate messagingTemplate;
 
     public void sendFCMNotification(String token, String title, String body) {
         Message message = Message.builder()
@@ -22,6 +27,7 @@ public class FCMService {
         try {
             var response = FirebaseMessaging.getInstance().sendAsync(message);
             log.info("Successfully Sent FCM Notification: " + response);
+            messagingTemplate.convertAndSend("/topic/notifications", body);
 
         } catch (Exception e) {
             log.error(body, e);
