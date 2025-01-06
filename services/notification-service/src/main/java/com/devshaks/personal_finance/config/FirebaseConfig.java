7b6cd4dd49d5ba7b2f6c1ee7b9946f318be4f969
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-06 20:03:10 +0000

    updated config

--- services/notification-service/src/main/java/com/devshaks/personal_finance/config/FirebaseConfig.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/config/FirebaseConfig.java
@@ -8,13 +8,29 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @Configuration
 public class FirebaseConfig {
 
     public FirebaseConfig() throws Exception {
-        FileInputStream serviceAccount = new FileInputStream(""); // get the json from firebase!
-        FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-        FirebaseApp.initializeApp(options);
+        try {
+            Dotenv dotenv = Dotenv.load();
+            String firebaseCredentials = dotenv.get("FIREBASE_CREDENTIALS");
+
+            if (firebaseCredentials == null || firebaseCredentials.isEmpty()) {
+                throw new IllegalArgumentException("FIREBASE_CREDENTIALS environment variable is not set.");
+            }
+
+            FileInputStream serviceAccount = new FileInputStream(firebaseCredentials);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error Fetching FCM Configuration", e);
+        }
+
     }
 }
