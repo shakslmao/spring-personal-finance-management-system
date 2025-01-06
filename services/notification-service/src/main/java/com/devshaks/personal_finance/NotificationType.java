665146b1a11bb1c04954e638d4b2cb49e0db3bc2
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-06 15:36:25 +0000

    type & status enums

---
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/NotificationType.java
@@ -0,0 +1,5 @@
+package com.devshaks.personal_finance;
+
+public enum NotificationType {
+    EMAIL, SMS, PUSH, IN_APP
+}
---
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationStatus.java
@@ -0,0 +1,5 @@
+package com.devshaks.personal_finance.notifications;
+
+public enum NotificationStatus {
+    PENDING, SENT, FAILED, READ
+}
