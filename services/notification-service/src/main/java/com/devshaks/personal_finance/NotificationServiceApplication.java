5b658a3b5190f590fe95b88501feea524bbdb255
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-07 16:44:13 +0000

    added env keys to main

--- services/notification-service/src/main/java/com/devshaks/personal_finance/NotificationServiceApplication.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/NotificationServiceApplication.java
@@ -15,6 +15,8 @@ public class NotificationServiceApplication {
         Dotenv dotenv = Dotenv.load();
         System.setProperty("MONGO_INITDB_ROOT_USERNAME", dotenv.get("MONGO_INITDB_ROOT_USERNAME"));
         System.setProperty("MONGO_INITDB_ROOT_PASSWORD", dotenv.get("MONGO_INITDB_ROOT_PASSWORD"));
+        System.setProperty("SES_ACCESS_KEY", dotenv.get("SES_ACCESS_KEY"));
+        System.setProperty("SES_SECRET_ACCESS_KEY", dotenv.get("SES_SECRET_ACCESS_KEY"));
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 }
