4dc0db53f616c20b66af92bf97b4f91b2d071e5f
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-09 22:36:43 +0000

    notification repository impl

--- services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationRepository.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationRepository.java
@@ -1,7 +1,12 @@
 package com.devshaks.personal_finance.notifications;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface NotificationRepository extends MongoRepository<Notification, String> {
+    List<Notification> findByRecipientId(String recipientId);
+
+    List<Notification> findByRecipientIdAndNotificationStatus(String recipientId, NotificationStatus status);
 
 }
