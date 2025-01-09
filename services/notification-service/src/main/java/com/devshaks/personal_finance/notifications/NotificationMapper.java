ee1e160994fce78a151cf493d1e6eb5e70ba56d4
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-09 22:36:34 +0000

    notification mapper with each notif type

--- services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationMapper.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationMapper.java
@@ -1,8 +1,53 @@
 package com.devshaks.personal_finance.notifications;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
+
+import com.devshaks.personal_finance.email.EmailNotificationRequest;
+import com.devshaks.personal_finance.pushnotification.PushNotificationRequest;
+import com.devshaks.personal_finance.sms.SMSNotificationRequest;
 
 @Service
 public class NotificationMapper {
+    public Notification toNewPushNotification(PushNotificationRequest pushRequest) {
+        return Notification.builder()
+                .recipientId(pushRequest.deviceToken())
+                .notificationType(NotificationType.PUSH)
+                .notificationMessage(pushRequest.message())
+                .metaData(Map.of("title", pushRequest.title()))
+                .notificationStatus(NotificationStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
 
+    public Notification toNewEmailNotification(EmailNotificationRequest emailRequest) {
+        return Notification.builder()
+                .recipientId(emailRequest.to())
+                .notificationType(NotificationType.EMAIL)
+                .notificationMessage(emailRequest.body())
+                .metaData(Map.of("from", emailRequest.from()))
+                .notificationStatus(NotificationStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public Notification toNewSMSNotification(SMSNotificationRequest smsRequest) {
+        return Notification.builder()
+                .recipientId(smsRequest.to())
+                .notificationType(NotificationType.SMS)
+                .notificationMessage(smsRequest.body())
+                .notificationStatus(NotificationStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public NotificationResponse mapToNotificationResponse(Notification notification) {
+        return new NotificationResponse(notification.getId(), notification.getRecipientId(),
+                notification.getNotificationType(), notification.getNotificationMessage(),
+                notification.getNotificationStatus(), notification.getCreatedAt(), notification.getSentAt(),
+                notification.getReadAt());
+
+    }
 }
