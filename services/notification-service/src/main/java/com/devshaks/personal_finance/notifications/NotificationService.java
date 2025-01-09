9209fa583680700acea0673022fbdf32b82f02ee
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-09 23:15:37 +0000

    updated service

--- services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationService.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/notifications/NotificationService.java
@@ -1,6 +1,7 @@
 package com.devshaks.personal_finance.notifications;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,9 @@ import com.devshaks.personal_finance.pushnotification.PushNotificationRequest;
 import com.devshaks.personal_finance.sms.SMSNotificationRequest;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
@@ -18,21 +21,59 @@ public class NotificationService {
     private final NotificationMapper notificationMapper;
 
     public NotificationResponse createPushNotification(PushNotificationRequest request) {
-        Notification notification = notificationMapper.toNewPushNotification(request);
-        Notification savedNotification = notificationRepository.save(notification);
-        return notificationMapper.mapToNotificationResponse(savedNotification);
+        return saveAndRespond(() -> notificationMapper.toNewPushNotification(request));
+
+        /*
+         * Notification notification =
+         * notificationMapper.toNewPushNotification(request);
+         * Notification savedNotification = notificationRepository.save(notification);
+         * return notificationMapper.mapToNotificationResponse(savedNotification);
+         */
     }
 
     public NotificationResponse createEmailNotification(EmailNotificationRequest request) {
-        Notification notification = notificationMapper.toNewEmailNotification(request);
-        Notification savedNotification = notificationRepository.save(notification);
-        return notificationMapper.mapToNotificationResponse(savedNotification);
+        return saveAndRespond(() -> notificationMapper.toNewEmailNotification(request));
+
+        /*
+         * Notification notification =
+         * notificationMapper.toNewEmailNotification(request);
+         * Notification savedNotification = notificationRepository.save(notification);
+         * return notificationMapper.mapToNotificationResponse(savedNotification);
+         */
     }
 
     public NotificationResponse createSMSNotification(SMSNotificationRequest request) {
-        Notification notification = notificationMapper.toNewSMSNotification(request);
-        Notification savedNotification = notificationRepository.save(notification);
-        return notificationMapper.mapToNotificationResponse(savedNotification);
+        return saveAndRespond(() -> notificationMapper.toNewSMSNotification(request));
+
+        /*
+         * Notification notification = notificationMapper.toNewSMSNotification(request);
+         * Notification savedNotification = notificationRepository.save(notification);
+         * return notificationMapper.mapToNotificationResponse(savedNotification);
+         */
+    }
+
+    private NotificationResponse saveAndRespond(Supplier<Notification> notificationSupplier) {
+        try {
+            Notification notification = notificationSupplier.get();
+            validateNotification(notification);
+            Notification savedNotification = notificationRepository.save(notification);
+            return notificationMapper.mapToNotificationResponse(savedNotification);
+
+        } catch (Exception e) {
+            log.error("Failed to create Notification: {}", e.getMessage());
+            throw new RuntimeException("Error, Notification Creation Failed", e);
+        }
+    }
+
+    private void validateNotification(Notification notification) {
+        if (notification.getRecipientId() == null || notification.getRecipientId().isEmpty()) {
+            throw new IllegalArgumentException("Recepient ID Must not be Null or Empty!");
+        }
+
+        if (notification.getNotificationMessage() == null || notification.getNotificationMessage().isEmpty()) {
+            throw new IllegalArgumentException("Message Must have a Body to Send!");
+        }
+
     }
 
     public List<NotificationResponse> getNotificationsByRecipient(String recipientId, NotificationStatus status) {
