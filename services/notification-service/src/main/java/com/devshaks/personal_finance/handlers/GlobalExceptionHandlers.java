1925d5f70e1c4c575c5e2a9839f3cfa80778ce3f
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-12 18:34:41 +0000

    handlers

--- services/notification-service/src/main/java/com/devshaks/personal_finance/handlers/GlobalExceptionHandlers.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/handlers/GlobalExceptionHandlers.java
@@ -1,6 +1,7 @@
 package com.devshaks.personal_finance.handlers;
 
-import com.devshaks.personal_finance.exceptions.*;
+import com.devshaks.personal_finance.exceptions.SESEmailException;
+import com.devshaks.personal_finance.exceptions.TwilioSMSException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,33 +13,16 @@ import java.util.HashMap;
 
 @RestControllerAdvice
 public class GlobalExceptionHandlers {
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleException(UserNotFoundException e) {
+
+    @ExceptionHandler(TwilioSMSException.class)
+    public ResponseEntity<String> handleException(TwilioSMSException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(e.getMessage());
     }
 
-    @ExceptionHandler(TransactionNotFoundException.class)
-    public ResponseEntity<String> handleException(TransactionNotFoundException e) {
+    @ExceptionHandler(SESEmailException.class)
+    public ResponseEntity<String> handleException(SESEmailException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(e.getMessage());
-    }
-
-    @ExceptionHandler(PaymentValidationException.class)
-    public ResponseEntity<String> handleException(PaymentValidationException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(e.getMessage());
-    }
-
-    @ExceptionHandler(BudgetExceededException.class)
-    public ResponseEntity<String> handleException(BudgetExceededException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(e.getMessage());
-    }
-
-    @ExceptionHandler(BudgetNotFoundException.class)
-    public ResponseEntity<String> handleException(BudgetNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(e.getMessage());
     }
 
@@ -47,11 +31,10 @@ public class GlobalExceptionHandlers {
         var errors = new HashMap<String, String>();
         e.getBindingResult().getAllErrors()
                 .forEach(error -> {
-                    var fieldName = ((FieldError)error).getField();
+                    var fieldName = ((FieldError) error).getField();
                     var errorMessage = error.getDefaultMessage();
                     errors.put(fieldName, errorMessage);
                 });
-
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors));
     }
 }
