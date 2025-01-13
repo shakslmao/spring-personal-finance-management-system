bda21fd0d3630c82461107c78b7bc8c4e32c8da6
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-13 12:17:24 +0000

    updated service for twilio

--- services/notification-service/src/main/java/com/devshaks/personal_finance/sms/TwilioSMSService.java
+++ services/notification-service/src/main/java/com/devshaks/personal_finance/sms/TwilioSMSService.java
@@ -1,6 +1,7 @@
 package com.devshaks.personal_finance.sms;
 
 import com.devshaks.personal_finance.config.TwilioConfig;
+import com.devshaks.personal_finance.exceptions.TwilioSMSException;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import lombok.RequiredArgsConstructor;
@@ -11,19 +12,26 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class TwilioSMSService {
-
     private final TwilioConfig twilioConfig;
 
     public void sendSMS(String to, String body) {
         log.info("Preparing SMS to: {}: {}", to, body);
         try {
-            Message message = Message
-                    .creator(new PhoneNumber(to), new PhoneNumber(twilioConfig.getTwilioPhoneNumber()), body).create();
+            String formattedPhoneNumber = validateAndFormatPhoneNumber(to);
+            Message message = Message.creator(
+                    new PhoneNumber(formattedPhoneNumber),
+                    new PhoneNumber(twilioConfig.getTwilioPhoneNumber()), body).create();
             log.info("Sending SMS. SID: {}", message.getSid());
         } catch (Exception e) {
             log.error("Failed to send SMS to {}. Error: {}", to, e.getMessage(), e);
-            throw new RuntimeException("Failed to send SMS", e);
+            throw new TwilioSMSException("Failed to send SMS");
         }
     }
 
+    private String validateAndFormatPhoneNumber(String phoneNumber) {
+        if (!phoneNumber.startsWith("+")) {
+            throw new TwilioSMSException("Invalid phone number: " + phoneNumber);
+        }
+        return phoneNumber.replaceAll("[^+0-9]", "");
+    }
 }
