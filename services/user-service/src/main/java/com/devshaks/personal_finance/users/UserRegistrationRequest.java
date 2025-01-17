ad20ecfcedeb1eaa1bb8fce95e25b300c3f398e3
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-17 17:52:23 +0000

    Updated Registration Request, including last name is new param

--- services/user-service/src/main/java/com/devshaks/personal_finance/users/UserRegistrationRequest.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/users/UserRegistrationRequest.java
@@ -11,10 +11,8 @@ import java.time.LocalDate;
 public record UserRegistrationRequest(
 
         @NotNull(message = "First Name is Required") String firstname,
-
+        @NotNull(message = "Last Name is Required") String lastname,
         @NotNull(message = "Email is Required") @Email(message = "Email is Invalid") String email,
-
         @NotNull(message = "Password is Required") @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters") @Pattern(regexp = ".*[0-9].*", message = "Password must contain at least one number") @Pattern(regexp = ".*[0-9].*", message = "Password must contain at least one number") @Pattern(regexp = ".*[@$!%*#?&].*", message = "Password must contain at least one special character") String password,
-
         @NotNull(message = "Date of Birth is Required") @Past(message = "Date of Birth must be in the past") LocalDate dateOfBirth) {
 }
