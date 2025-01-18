035326c9ee2233f3ebc2400dbe2dbbd0ec803894
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-18 21:11:20 +0000

    user details impl, load by username

---
+++ services/user-service/src/main/java/com/devshaks/personal_finance/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.devshaks.personal_finance.security;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devshaks.personal_finance.users.UserRepository;;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + userEmail));
+    }
+}
