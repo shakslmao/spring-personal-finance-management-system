3f85af3f5e1bdc785dae80992bbb926386a26799
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-18 21:11:27 +0000

    global handlers

--- services/user-service/src/main/java/com/devshaks/personal_finance/handlers/GlobalExceptionHandlers.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/handlers/GlobalExceptionHandlers.java
@@ -3,6 +3,10 @@ package com.devshaks.personal_finance.handlers;
 import com.devshaks.personal_finance.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.MessagingException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -54,12 +58,63 @@ public class GlobalExceptionHandlers {
                 .body(e.getMessage());
     }
 
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(LockedException exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ExceptionResponse.builder()
+                        .businessErrorCode(BusinessErrorCodes.ACCOUNT_LOCKED.getCode())
+                        .businessErrorDescription(BusinessErrorCodes.ACCOUNT_LOCKED.getDescription())
+                        .error(exception.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<ExceptionResponse> handleException(DisabledException exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ExceptionResponse.builder()
+                        .businessErrorCode(BusinessErrorCodes.ACCOUNT_DISABLED.getCode())
+                        .businessErrorDescription(BusinessErrorCodes.ACCOUNT_DISABLED.getDescription())
+                        .error(exception.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> handleException(BadCredentialsException exception) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ExceptionResponse.builder()
+                        .businessErrorCode(BusinessErrorCodes.BAD_CREDENTIALS.getCode())
+                        .businessErrorDescription(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
+                        .error(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
+                        .build());
+    }
+
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<ExceptionResponse> handleException(MessagingException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ExceptionResponse.builder()
+                        .error(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
+                        .build());
+    }
+
+    @ExceptionHandler(OperationNotPermittedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(OperationNotPermittedException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionResponse.builder()
+                        .error(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
+                        .build());
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException e) {
         var errors = new HashMap<String, String>();
         e.getBindingResult().getAllErrors()
                 .forEach(error -> {
-                    var fieldName = ((FieldError)error).getField();
+                    var fieldName = ((FieldError) error).getField();
                     var errorMessage = error.getDefaultMessage();
                     errors.put(fieldName, errorMessage);
                 });
