0f88412c9e52c24b4c75b1163bf44a7905991352
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-19 14:48:05 +0000

    new methods

---
+++ services/user-service/src/main/java/com/devshaks/personal_finance/security/JwtFilter.java
@@ -0,0 +1,11 @@
+package com.devshaks.personal_finance.security;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class JwtFilter {
+
+}
