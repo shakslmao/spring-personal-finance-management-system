dde7553039fab2a9d30bde3b62c54b32a542b325
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-20 18:14:25 +0000

    updates

--- services/user-service/src/main/java/com/devshaks/personal_finance/UserServiceApplication.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/UserServiceApplication.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
+@EnableAsync
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication
 public class UserServiceApplication {
