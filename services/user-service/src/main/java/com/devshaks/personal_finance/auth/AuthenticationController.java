194e74139b1b68c1f8cf51841e760fd6990d7f0a
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-20 18:14:48 +0000

    changes

--- services/user-service/src/main/java/com/devshaks/personal_finance/auth/AuthenticationController.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/auth/AuthenticationController.java
@@ -4,7 +4,6 @@ import java.net.URI;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.MessagingException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +17,7 @@ import com.devshaks.personal_finance.users.UserDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
--- services/user-service/src/main/java/com/devshaks/personal_finance/token/TokensRepository.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/token/TokensRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ch.qos.logback.core.subst.Token;
 
 public interface TokensRepository extends JpaRepository<Tokens, Long> {
     Optional<Tokens> findByToken(String token);
