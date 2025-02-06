5162de32de8414f0a527257c435422d7facbba97
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-06 13:33:08 +0000

    user security config changes

--- services/user-service/src/main/java/com/devshaks/personal_finance/security/SecurityConfiguration.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/security/SecurityConfiguration.java
@@ -26,18 +26,16 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors(cors -> cors.configurationSource(request -> {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.addAllowedOrigin("http://localhost:3000");
+            configuration.addAllowedOriginPattern("*");
             configuration.addAllowedMethod("*");
             configuration.addAllowedHeader("*");
             configuration.setAllowCredentials(true);
             configuration.addExposedHeader(HttpHeaders.SET_COOKIE);
-            return  configuration;
+            return configuration;
         }))
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> req.requestMatchers("/api/v1/auth/**", "/v2/api-docs",
-                        "/v3/api-docs", "/swagger-resources", "/swagger-resources/**",
-                        "/configuration/ui", "/configuration/security", "/swagger-ui/**",
-                        "/webjars/**", "/swagger-ui.html").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(
+                        req -> req.requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
