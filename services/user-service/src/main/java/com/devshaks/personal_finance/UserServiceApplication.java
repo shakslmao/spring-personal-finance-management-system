024a9ac25182360ec3c75877c16badaf147f5019
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-10 18:08:23 +0000

    secret key gen

--- services/user-service/src/main/java/com/devshaks/personal_finance/UserServiceApplication.java
+++ services/user-service/src/main/java/com/devshaks/personal_finance/UserServiceApplication.java
@@ -1,11 +1,16 @@
 package com.devshaks.personal_finance;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
 
 @Slf4j
 @EnableAsync
@@ -18,6 +23,13 @@ public class UserServiceApplication {
         System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
         System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
         System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+
+        /*
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String base64Key = Base64.getUrlEncoder().withoutPadding().encodeToString(key.getEncoded());
+        System.out.println("BASE64 KEY: " + base64Key);
+        */
+
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
